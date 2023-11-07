31dba1d33ad49b1f0aefe807737ba0add0ebaf7a
alzabid <alzabid.ashik@gmail.com>
2023-11-07 22:27:17 +0600

    added filter functionality

--- src/pages/Assignments.jsx
+++ src/pages/Assignments.jsx
@@ -5,21 +5,19 @@ import Swal from "sweetalert2";
 
 const Assignments = () => {
   const [assignments, setAssignments] = useState([]);
-  const [categoryFilter, setCategoryFilter] = useState("");
-  // const url = "http://localhost:5000/assignments";
+  const [selectLevel, setSelectLevel] = useState("All");
+
+  const url = "http://localhost:5000/assignments";
   useEffect(() => {
-    const url = `http://localhost:5000/assignments${
-      categoryFilter ? `?category=${categoryFilter}` : ""
-    }`;
+    // const url = "http://localhost:5000/assignments";
     fetch(url)
       .then((res) => res.json())
       .then((data) => setAssignments(data));
-  }, [categoryFilter]);
+  }, []);
 
-  // const handleFilter = (event) =>{
-  //   const level = event.target.value;
-  //   console.log(level)
-  // }
+  const filterItem = assignments.filter((x) => {
+    return selectLevel === "All" || x.level === selectLevel;
+  });
 
   const handleDelete = (_id) => {
     console.log(_id);
@@ -57,29 +55,33 @@ const Assignments = () => {
     <div>
       <Container>
         <div className="flex justify-center items-center mt-10 gap-5">
-          <h1 className="text-2xl">Filter by Difficulty Level: </h1>
+          <h1 className="md:text-2xl">Filter by Difficulty Level: </h1>
           <select
-            value={categoryFilter}
-            onChange={(e) => setCategoryFilter(e.target.value)}
-            className="select select-bordered w-full max-w-xs"
+            onChange={(e) => setSelectLevel(e.target.value)}
+            value={selectLevel}
+            className="select select-bordered w-32 md:w-full max-w-xs"
           >
-            <option value="" disabled selected>
-              Select One
-            </option>
+            <option value="All">Select All</option>
             <option value="Easy">Easy</option>
             <option value="Medium">Medium</option>
             <option value="Hard">Hard</option>
           </select>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6 py-10">
-          {assignments.map((element) => (
-            <AssignmentsCard
-              key={element._id}
-              element={element}
-              handleDelete={handleDelete}
-            ></AssignmentsCard>
-          ))}
-        </div>
+        {filterItem.length === 0 ? (
+          <div className=" h-[80vh] flex justify-center items-center">
+            <p className="md:text-2xl">No Assignment found for the selected level.</p>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6 py-10">
+            {filterItem.map((element) => (
+              <AssignmentsCard
+                key={element._id}
+                element={element}
+                handleDelete={handleDelete}
+              ></AssignmentsCard>
+            ))}
+          </div>
+        )}
       </Container>
     </div>
   );
